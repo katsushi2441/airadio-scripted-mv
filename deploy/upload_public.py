23c254f070012d757a04d2d1e489e74a817c0a0c
katsushi2441 <katsushi2441@gmail.com>
2026-05-27 20:31:14 +0900

    Add Scripted MV project icon

--- deploy/upload_public.py
+++ deploy/upload_public.py
@@ -28,6 +28,9 @@ def main() -> None:
         "config.php",
         "assets/kurage-icon.png",
         "assets/kurage.png",
+        "assets/scripted-mv-icon.png",
+        "assets/scripted-mv-icon-512.png",
+        "assets/scripted-mv-icon-256.png",
     ]
     with ftplib.FTP(host, timeout=60) as ftp:
         ftp.login(user, password)
